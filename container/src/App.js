fac6edc12f0e9874009f22b1aed3c67774ad585c
briha <hello@briha.xyz>
2024-02-18 01:47:32 +0530

    randomised prod ui classnames

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,24 @@
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
